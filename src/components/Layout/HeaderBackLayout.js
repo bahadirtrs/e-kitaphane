096886d9ebbee18d558988c39f5ca436e6ceeb4d
Bahadır TIRAŞ <otiras.mdbf17@iste.edu.tr>
2021-05-01 01:04:59 +0300

    headerDesing

--- src/components/Layout/HeaderBackLayout.js
+++ src/components/Layout/HeaderBackLayout.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import { Text } from 'react-native'
-import { View, Dimensions, TouchableOpacity } from 'react-native'
+import { View, Dimensions, TouchableOpacity,StyleSheet } from 'react-native'
 import Icon from "react-native-vector-icons/Ionicons"
 export default function HeaderBackLayout({butonPress,butonPressRight,butonColor,pageName}) {
     return (
-    <View style={{ zIndex:1, width:Dimensions.get('screen').width, flexDirection:'row', justifyContent:'space-between',alignItems:'center', paddingHorizontal:15, paddingVertical:5}} >
-        <TouchableOpacity style={{  flexDirection:'row', justifyContent:'space-between', alignItems:'flex-end'}} onPress={butonPress} >
+    <View style={StyleSheet.container} >
+        <TouchableOpacity style={styles.buttonStyle} onPress={butonPress} >
             <Icon name="chevron-back-outline" size={30} color={butonColor?butonColor:"#fff" }/> 
         </TouchableOpacity>
-        <Text style={{fontFamily:'GoogleSans-Medium', color:butonColor?butonColor:"#fff", fontSize:16}} >{pageName}</Text>
-        <TouchableOpacity style={{  flexDirection:'row', justifyContent:'space-between', alignItems:'flex-end'}} onPress={butonPressRight} >
+        <Text style={
+            [styles.pageNameStyle,{color:butonColor?butonColor:"#fff", }]}>{pageName}</Text>
+        <TouchableOpacity style={styles.buttonStyle} onPress={butonPressRight} >
             <Icon name="help-circle-outline" size={30} color={butonColor?butonColor:"#fff" }/> 
         </TouchableOpacity>
-       
     </View>
     )
 }
+const styles = StyleSheet.create({
+    container:{ 
+        zIndex:1, 
+        width:Dimensions.get('screen').width, 
+        flexDirection:'row', 
+        justifyContent:'space-between',
+        alignItems:'center', 
+        paddingHorizontal:15, 
+        paddingVertical:5
+    },
+    buttonStyle:{  
+        flexDirection:'row', 
+        justifyContent:'space-between', 
+        alignItems:'flex-end'
+    },
+    pageNameStyle:{
+        fontFamily:'GoogleSans-Medium', 
+        fontSize:16}
+})
