085e53df6518f1654c02df27f9e5666dadaf93bc
Bahadır TIRAŞ <otiras.mdbf17@iste.edu.tr>
2021-04-30 14:42:59 +0300

    deneme

--- src/rotues/Account/AccountScreen.js
+++ src/rotues/Account/AccountScreen.js
@@ -25,7 +25,7 @@ export default function AccountScreen({navigation}) {
                 <View style={{flex:2,paddingBottom:20, justifyContent:'center', alignItems:'center'}} >
                     <SubmitButton butonPress={()=>navigation.push('LogIn')} />
                     <TextButton
-                        questions={'Henüz hesap oluşturmadınız mı?'}
+                        questions={'Henüz hesap oluştugfrmadınız mı?'}
                         redirectText={'Hesap Oluşturun'}
                         buttonPress={()=> navigation.push('SingIn')}
                     />
